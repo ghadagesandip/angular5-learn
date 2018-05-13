adbcad61cb94eed6b21361316574298cdb1d810b
Sandip Ghadge <sandip.ghadge@wwindia.com>
2018-05-13 17:44:25 +0400

    intial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: HERO;
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  onSelect(hero: HERO): void {
+    this.selectedHero = hero;
+  }
+
+
+
+
+}
